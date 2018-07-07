10e0dd6e954f3fc60e63d75feb899bb321a9a346
gvpk.chandra@gmail.com <gvpk.chandra@gmail.com>
2018-07-07 18:36:07 +0530

    Added solution for reversing bits - typo correction

--- epi_judge_cpp/reverse_bits.cc
+++ epi_judge_cpp/reverse_bits.cc
@@ -1,7 +1,29 @@
 #include "test_framework/generic_test.h"
 unsigned long long ReverseBits(unsigned long long x) {
   // TODO - you fill in here.
-  return 0;
+  unsigned int lsb = 0;
+  unsigned int msb = (8 * sizeof(unsigned long long)) - 1;
+
+  // Number to be returned.
+  auto ret_num = x;
+
+  while(lsb < msb) {
+    // We need to toggle only when they are different.
+    if(((ret_num >> lsb) & 0x1) != ((ret_num >> msb) & 0x1)) {
+
+      // Form a bit mask to swap.
+      unsigned long long bit_mask = ((static_cast<unsigned long long>(0x1) << lsb) |
+                                (static_cast<unsigned long long>(0x1) << msb));
+
+      // Number to be returned.
+      ret_num ^= bit_mask;
+    }
+
+    lsb++;
+    msb--;
+  }
+
+  return ret_num;
 }
 
 int main(int argc, char* argv[]) {
